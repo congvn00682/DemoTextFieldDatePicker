b0f525f93e7b1e9706a3cf33c19272c82b0336a7
Vu Ngoc Cong <congvn00682@gmail.com>
2018-07-24 11:37:51 +0700

    Demo select data from DatePicker to display Textfield

--- DemoTextFieldDatePicker/ViewController.swift
+++ DemoTextFieldDatePicker/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtInputDatePicker: UITextField!
+    let datePicker = UIDatePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        createDatePicker()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +25,33 @@ class ViewController: UIViewController {
     }
 
 
+    // design show Picker on UI
+    func createDatePicker() {
+        
+        datePicker.datePickerMode = .date
+        // enable picker
+        txtInputDatePicker.inputView = datePicker
+        
+        // create a toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        // add button on this toolbar
+        let btnDone = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneOnClicked))
+        toolbar.setItems([btnDone], animated: true)
+        
+        txtInputDatePicker.inputAccessoryView = toolbar
+        
+    }
+    
+    // get data from Picker
+    @objc func doneOnClicked() {
+        // format the date display in textField
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        
+        txtInputDatePicker.text = dateFormatter.string(from: datePicker.date);        self.view.endEditing(true)
+    }
 }
 
